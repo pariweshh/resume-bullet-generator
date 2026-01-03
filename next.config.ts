2c8ae0d3a0b1e25d852bb0718c89cb7ac4dba1d0
pariwesh <pariweshtamrakar@gmail.com>
2026-01-03 15:38:10 +1100

    initial commit with global styling and layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
-export default nextConfig;
+  cacheComponents: true,
+
+  // reactCompiler: true,
+
+  images: {
+    minimumCacheTTL: 14400,
+    qualities: [75],
+    remotePatterns: [],
+  },
+
+  env: {
+    NEXT_PUBLIC_APP_URL:
+      process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  },
+
+  // Headers for security
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
